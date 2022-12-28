f724c8272c096062698fc62a1fd99852cd8d24c4
Augustine <augustine.normanyo@amalitech.org>
2022-12-28 15:24:13 -0800

    phase1

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,27 @@
 import React from 'react';
-import './App.css';
+import './App.css'
+import './Body.css'
+import Titles from './component/Title'
+import Body from './component/Body';
 
 
 function App() {
+
+  let state = {
+    labels: ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'],
+
+    datasets: [
+      {
+        label: "",
+        backgroundColor: 'hsl(10, 79%, 65%)',
+        hoverBackgroundColor: 'hsl(186, 34%, 60%)',
+        pointerStyle:'line',
+        borderRadius: 8,
+        data: [17.45, 34.91, 52.36, 31.07, 23.39, 43.28, 25.48]
+      }
+    ]
+  }
+
   return (
     <div className="App">
 
@@ -10,7 +29,8 @@ function App() {
 
          <div className="banner">
 
-           
+           <Titles />
+           <Body state={state}/>  
 
          </div>
 
--- src/component/Title.tsx
+++ src/component/Title.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Title() {
   return (
     <div>
-        Title
+       
     </div>
   )
 }
